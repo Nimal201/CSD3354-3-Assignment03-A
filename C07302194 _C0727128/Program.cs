687fce2decfff529b80aed4d4df109069dbb0e6c
visualstudio <visualstudio@122-PC39>
2019-03-15 18:11:46 -0400

    Commit1

---
+++ C07302194 _C0727128/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07302194__C0727128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
